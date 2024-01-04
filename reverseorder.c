406afbce4d1c5e1fd00158881595c3d234c9eaae
sacchitsharma2945 <146860176+sacchitsharma2945@users.noreply.github.com>
2024-01-04 21:48:42 +0530

    Add files via upload

---
+++ reverseorder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num,i;
+
+    
+    scanf("%d", &num);
+
+    while (n!=0)
+     {
+        int number % 10;
+        reverse = reverse * 10 + digit;
+        number = number / 10;
+    }
+
+    printf("The number in reverse order is: %d\n", reverse);
+
+    return 0;
+}
